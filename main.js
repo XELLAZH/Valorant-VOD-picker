5d488a58351813f61d41fa5cfd11961dd6e20584
XELLAZH <zhuchkovsasha2004@yandex.ru>
2025-05-16 16:13:34 +0300

    Added simple agent list (Jett only yet) and photos for players

--- main.js
+++ main.js
@@ -14,6 +14,20 @@ const agentsData = {
     ]
 }
 
+// ./imgs/Tenz.jpg
+const imgsPaths = {
+    "ZmjjKK" : "./imgs/ZmjjKK.jpg",
+    "Derke" : "./imgs/Derke.jpg",
+    "Chichoo" : "./imgs/Chichoo.jpg",
+    "Zekken" : "./imgs/Zekken.jpg",
+    "TenZ" : "./imgs/TenZ.jpg",
+    "aspas" : "./imgs/aspas.jpg",
+    "t3xture" : "./imgs/t3xture.jpg",
+    "Meteor" : "./imgs/Meteor.jpg",
+    "WoOt" : "./imgs/WoOt.jpg",
+    "f0rsakeN" : "./imgs/f0rsakeN.jpg",
+}
+
 function getRandomIndex(maxNum) {
     return Math.floor(Math.random() * maxNum)
 }
@@ -27,6 +41,7 @@ const pickVodButton = document.getElementById('pickVodBtn');
 pickVodButton.addEventListener('click', () => {
     
     let pick = agentsData.JETT[getRandomIndex(agentsData.JETT.length)];
-    messagePar.innerHTML = `Посмотри демо ${pick}<br><a href="http://www.youtube.com/watch?v=ofc0FE7hgEI&list=PLyUTX5LGxnvD5MOXr0q0XBoG52pYdUNcc">Ссылка на плейлист с VOD</a>`;
+    let path = imgsPaths[pick]
+    messagePar.innerHTML = `<img src="${path}" alt="${pick} photo" style="width: 240px;"><br>Посмотри демо ${pick}<br><a href="http://www.youtube.com/watch?v=ofc0FE7hgEI&list=PLyUTX5LGxnvD5MOXr0q0XBoG52pYdUNcc">Ссылка на плейлист с VOD</a>`;
 });
 
